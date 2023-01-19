85503ab1df9782f2496ce6dfa8dd4c32632695a9
taesubhan <tshan499@gmail.com>
2023-01-18 20:03:07 -0800

    Added UI to game so users can interact with site instead of console
    html and Javascript files have been updated to include a UI component
    to the game. 3 buttons with event listeners have been added, which
    would indicate the user's input prompt. Elements with text have been
    appended into the DOM through Javascript to indicate the round # and
    winner of that round. Player and computer points and round count are
    tracked using global variables.
    Once 5 points have been reached by either player or computer, the
    following will occur:
    - A final message will be inserted into DOM
    - "Play Again" button will be inserted into DOM
    "Play Again" will reset the scoreboard and message board to its
    original state.

--- game-logic.js
+++ game-logic.js
@@ -1,19 +1,15 @@
+let roundCount = 0;
+let playerPoint = 0;
+let computerPoint = 0;
+
 function getRandomNum(max) {
     return Math.floor(Math.random() * max) + 1;
 }
 
 function getComputerChoice() {
+/* Retrieves a randomized choice of rock, paper, or scissor */
     let rand = getRandomNum(3);
 
-    // if (rand === 1) {
-    //     return 'Rock';
-    // } else if (rand === 2) {
-    //     return 'Paper';
-    // } else if (rand === 3) {
-    //     return 'Scissors';
-    // } else {
-    //     return 'ERROR! Something went wrong';
-    // }
     switch (rand) {
         case 1:
             return 'Rock';
@@ -29,11 +25,8 @@ function getComputerChoice() {
     }
 }
 
-function askChoice() {
-    return prompt('Please type in: Rock, Paper, or Scissors');
-}
-
-function playRound(playerSelection, computerSelection) {
+function getResult(playerSelection, computerSelection) {
+/* Determines the winner for this round of the game */
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
 
@@ -74,39 +67,100 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function getWinner(player, computer) {
-    if (player > computer) {
-        return 'Player wins the game!';
-    } else if (player < computer) {
-        return 'Computer wins the game!';
-    } else if (player === computer) {
-        return 'Both Player and Computer tied!'
+function resetGame() {
+    roundCount = 0;
+    playerPoint = 0;
+    computerPoint = 0;
+
+    const pointsSpan = document.querySelectorAll('div.Points > div span');
+    pointsSpan.forEach(function(span) {span.textContent = ''});
+    console.log(pointsSpan);
+
+    const resultBox = document.querySelector('div.Result');
+    resultBox.textContent = '';
+
+    const playAgainButton = document.querySelector('button.Reset');
+    const body = document.querySelector('body');
+    body.removeChild(playAgainButton);
+
+    game();
+}
+
+function addResetButton() {
+    const playAgainButton = document.createElement('button');
+    playAgainButton.textContent = 'Play Again';
+    playAgainButton.classList.add('Reset');
+    const body = document.querySelector('body');
+    body.appendChild(playAgainButton);
+    playAgainButton.addEventListener('click', resetGame);
+}
+
+function endGame() {
+    const buttons = document.querySelectorAll('.RPS-buttons button');
+    buttons.forEach(function(button) {button.removeEventListener('click', handleButton)});
+    addResetButton();
+}
+
+function checkEnd(playerPoint, computerPoint, resultBox) {
+/* Determines whether game is over by checking if either player or computer has 5 points. If so, message is displayed */
+    if (playerPoint >= 5) {
+        const finalMessage = document.createElement('div');
+        finalMessage.textContent = 'Congratulations! You beat the computer in Rock-Paper-Scissors!';
+        resultBox.appendChild(finalMessage);
+        endGame();
+    }
+    else if (computerPoint >= 5) {
+        const finalMessage = document.createElement('div');
+        finalMessage.textContent = 'Game Over! You lost against the computer in Rock-Paper-Scissors!';
+        resultBox.appendChild(finalMessage);
+        endGame();
     }
 }
 
-function game() {
-    const roundMax = 5;
-    let playerScore = 0;
-    let computerScore = 0;
+function incrementRound() {
+    roundCount++;
+}
 
-    for (let roundCount = 1; roundCount <= roundMax; roundCount++) {
-        let result = playRound(askChoice(), getComputerChoice());
-        result = `Round ${roundCount}: ` + result; 
-        console.log(result);
-        if (result.includes('Win')) {
-            playerScore++;
-        } else if (result.includes('Lose')) {
-            computerScore++;
-        } else if (result.includes('Tied')) {
-            playerScore++;
-            computerScore++;
-        }
+function addRoundMessage(result) {
+    return `Round ${roundCount}: ${result}`;
+}
+
+function playRound(player, computer) {
+/*  */
+    let result = getResult(player, computer);
+
+    if (result.includes('Win')) {
+        playerPoint++;
+    }
+    else if (result.includes('Lose')) {
+        computerPoint++;
     }
 
-    let winnerMessage = getWinner(playerScore, computerScore);
-    let scoreboardMessage = `Player: ${playerScore} | Computer: ${computerScore}`;
-    let finalMessage = winnerMessage + '\n' + scoreboardMessage;
-    console.log(finalMessage);
+    incrementRound();
+    result = addRoundMessage(result);
+
+    const playerPointElem = document.querySelector('span.Player');
+    playerPointElem.textContent = playerPoint;
+    const computerPointElem = document.querySelector('span.Computer');
+    computerPointElem.textContent = computerPoint;
+
+    const resultBox = document.querySelector('div.Result');
+    resultBox.textContent = result;
+    // const responseLine = document.createElement('div');
+    // responseLine.textContent = result;
+    // resultBox.appendChild(responseLine);
+    
+
+    checkEnd(playerPoint, computerPoint, resultBox);
+}
+
+function handleButton(e) {
+    playRound(e.target.textContent, getComputerChoice());
+}
+
+function game() {
+    const buttons = document.querySelectorAll('.RPS-buttons button')
+    buttons.forEach(function(button) {button.addEventListener('click', handleButton)})
 }
 
 game();
